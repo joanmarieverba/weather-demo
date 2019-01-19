36be194df61ab58ba279c27c9fafc239f10fcf7b
Joan Marie Verba <verba001@umn.edu>
2019-01-19 13:50:47 -0600

    API call

--- weather.js
+++ weather.js
@@ -1,4 +1,10 @@
 "use strict"
 
-const url = http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=2ab5a5b18737e945b5af9cae2e8e1ffe;
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=2ab5a5b18737e945b5af9cae2e8e1ffe";
 
+$.ajax({
+    url: url,
+     success: function (result) {
+        console.log(result);
+    }
+});
