fc8e9ff15a1a85b7de26930aa8407d4ccdd1bc8c
Joan Marie Verba <verba001@umn.edu>
2019-01-19 15:45:21 -0600

    progress

--- weather.js
+++ weather.js
@@ -10,6 +10,9 @@ $.ajax({
         console.log("name", result.name);
         $("#location").text(result.name);
 
+        let F = Math.round(result.main.temp * (9 / 5) - 459.67);
+        let Fahrenheit = F.toString();
+        $("#temperature").text(Fahrenheit);
 
         $("#sky").text(result.weather[0].description);
 
