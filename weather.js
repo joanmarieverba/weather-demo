f1a274ae84e8de0d593207de68d66aeeccb3c453
Joan Marie Verba <verba001@umn.edu>
2019-01-20 15:41:56 -0600

    city

--- weather.js
+++ weather.js
@@ -7,12 +7,15 @@ $.ajax({
     success: function (result) {
         console.log(result);
     // Retrieve name of city
-        console.log("name", result.name);
-        $("#location").text(result.name);
+        let displayCity = `Current weather in ${result.name}`;
+        $("#location").html(displayCity);
     //Retrieve and calculate temperature
         let F = Math.round(result.main.temp * (9 / 5) - 459.67);
-        let Fahrenheit = F.toString();
-        $("#temperature").text(Fahrenheit);
+        // let Fahrenheit = F.toString();
+        // $("#temperature").text(Fahrenheit);
+        let displayTemp = `Temperature: ${F}&#176;F`;
+        $("#temperature").html(displayTemp);
+
     //Retrieve and calculate wind speed
         let windSpeed = Math.round(result.wind.speed / .44704)
         let wind = windSpeed.toString();
